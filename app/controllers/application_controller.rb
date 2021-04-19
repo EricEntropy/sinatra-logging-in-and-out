d00294521163f2d54b1fe8efd48f120a475b3b30
Eric <contreras.eric18@gmail.com>
2021-04-18 23:20:49 -0400

    commit tree fe7e944f3a4780a3bf3efae6670d920a46046a13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else 
+      erb :error
+    end
+    erb :account
   end
 
   get '/account' do
-
+    erb :logout
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
 
 end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+    def self.current_user(session)
+        id = session[:user_id]
+        current_user = User.find(id)
+        current_user
+    end 
+
+    # def self.is_logged_in?(session)
+    #     session.include?(:user_id)
+    # end 
 end
